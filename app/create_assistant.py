4ad141c9cc24ad697943676d62977466e84b87fe
Darec Stowell <dstowell@mysidewalk.com>
2024-12-21 22:27:30 -0600

    install jinja2, render instructions, setup modules. move eventHandler

--- app/create_assistant.py
+++ app/create_assistant.py
@@ -1,27 +1,15 @@
+import os
+
 from dotenv import load_dotenv
+from helpers import _render
+from openai import OpenAI
 
 load_dotenv()
 
-import os
-
-from openai import OpenAI
-
 openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-
-instructions = """You are an assistant running data analysis on CSV files.
-
-You will use code interpreter to run the analysis.
-
-However, instead of rendering the charts as images, you will generate a plotly figure and turn it into json.
-You will create a file for each json that I can download through annotations.
-"""
-
+instructions = _render.render_template("agent_instructions.jinja2")
 tools = [{"type": "code_interpreter"}, {"type": "file_search"}]
-
 file = openai_client.files.create(file=open("../data/all-wiki-pages-12-20-2024.txt", "rb"), purpose="assistants")
-
-
 assistant = openai_client.beta.assistants.create(
     model="gpt-4o",
     name="Wraeclast Whisperer",
@@ -31,9 +19,7 @@ assistant = openai_client.beta.assistants.create(
     tool_resources={"code_interpreter": {"file_ids": [file.id]}},
 )
 
-
 # Add the assistant id to the .env file
 with open(".env", "a") as f:
     f.write(f'OPENAI_ASSISTANT_ID = "{assistant.id}"\n')
-
 print(f"Assistant created with id: {assistant.id} and saved to .env file")
---
+++ app/helpers/_render.py
@@ -0,0 +1,15 @@
+import os
+
+import jinja2
+
+
+def render_template(template_name: str) -> str:
+    # Get the directory of the current script
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the path to the templates directory
+    templates_dir = os.path.join(file_dir, "..", "..", "templates")
+    # Load and render the instructions template
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_dir))
+    template = env.get_template(template_name)
+    instructions = template.render()
+    return instructions
